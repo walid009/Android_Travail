e372b0127d15ec9e930d1472676d28f88ecd4b9a
chekir walid <walidchekir98@gmail.com>
2022-01-27 10:49:46 -1100

    app lemonade complete

---
+++ app/src/main/java/tn/esprit/lemonade/MainActivity.kt
@@ -0,0 +1,168 @@
+package tn.esprit.lemonade
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * NE MODIFIEZ AUCUN NOM DE VARIABLE OU DE VALEUR OU LEURS VALEURS INITIALES.
+     *
+     * Tout ce qui est étiqueté var au lieu de val devrait être modifié dans les fonctions mais NE PAS
+     * modifier leurs valeurs initiales déclarées ici, cela pourrait empêcher l'application de fonctionner correctement.
+     */
+    private val LEMONADE_STATE = "LEMONADE_STATE"
+    private val LEMON_SIZE = "LEMON_SIZE"
+    private val SQUEEZE_COUNT = "SQUEEZE_COUNT"
+    // SELECT représente l'état "cueillir du citron"
+    private val SELECT = "select"
+    // SQUEEZE représente l'état "presser le citron"
+    private val SQUEEZE = "squeeze"
+    // DRINK représente l'état "boire de la limonade"
+    private val DRINK = "drink"
+    // RESTART représente l'état où la limonade a été bue et le verre est vide
+    private val RESTART = "restart"
+    // Par défaut l'état est à select
+    private var lemonadeState = "select"
+    // Taille de citron par défaut est à -1
+    private var lemonSize = -1
+    // Par défaut le squeezeCount est à -1
+    private var squeezeCount = -1
+
+    private var lemonTree = LemonTree()
+    private var lemonImage: ImageView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // === NE PAS MODIFIER LE CODE DANS LA DÉCLARATION IF SUIVANTE ===
+        if (savedInstanceState != null) {
+            lemonadeState = savedInstanceState.getString(LEMONADE_STATE, "select")
+            lemonSize = savedInstanceState.getInt(LEMON_SIZE, -1)
+            squeezeCount = savedInstanceState.getInt(SQUEEZE_COUNT, -1)
+        }
+        // === FIN IF ===
+
+        lemonImage = findViewById(R.id.image_lemon_state)
+        setViewElements()
+
+        lemonImage!!.setOnClickListener {
+            // TODO 1: appeler la méthode qui gère l'état lorsque l'image est cliquée
+            clickLemonImage()
+        }
+
+        lemonImage!!.setOnLongClickListener {
+            // TODO 2: remplacer 'false' par un appel à la fonction qui affiche le nombre de compressions
+            showSnackbar()
+        }
+    }
+
+    /**
+     * === NE PAS MODIFIER CETTE MÉTHODE ===
+     *
+     * Cette méthode enregistre l'état de l'application si elle est mise en arrière-plan.
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(LEMONADE_STATE, lemonadeState)
+        outState.putInt(LEMON_SIZE, lemonSize)
+        outState.putInt(SQUEEZE_COUNT, squeezeCount)
+        super.onSaveInstanceState(outState)
+    }
+
+    /**
+     * En cliquant, vous obtiendrez une réponse différente selon l'état.
+     * Cette méthode détermine l'état et procède à l'action correcte.
+     */
+    private fun clickLemonImage() {
+        var conditionSortie = false
+
+        if (lemonadeState == SELECT && !conditionSortie) {
+            lemonadeState = "squeeze"
+            val  lemonTree = LemonTree()
+            lemonSize = lemonTree.pick()
+            squeezeCount = 0
+            conditionSortie = true;
+        }
+        if (lemonadeState == SQUEEZE && !conditionSortie) {
+            if(lemonSize == 0){
+                lemonadeState = "drink"
+                lemonSize = -1
+                conditionSortie = true;
+            }else{
+                lemonSize--
+                squeezeCount++
+            }
+        }
+        if (lemonadeState == DRINK && !conditionSortie) {
+            lemonadeState = "restart"
+            conditionSortie = true;
+        }
+        if (lemonadeState == RESTART && !conditionSortie) {
+            lemonadeState = "select"
+            conditionSortie = true;
+        }
+        println(lemonadeState)
+
+        setViewElements()
+    }
+
+    /**
+     * Mettre en place les éléments d'affichage selon l'état.
+     */
+    private fun setViewElements() {
+        val textAction: TextView = findViewById(R.id.text_action)
+
+        if (lemonadeState == SELECT) {
+            lemonImage!!.setImageResource(R.drawable.lemon_tree)
+            textAction.setText(R.string.lemon_select)
+        }
+
+        if (lemonadeState == SQUEEZE) {
+            lemonImage!!.setImageResource(R.drawable.lemon_squeeze)
+            textAction.setText(R.string.lemon_squeeze)
+        }
+
+        if (lemonadeState == DRINK) {
+            lemonImage!!.setImageResource(R.drawable.lemon_drink)
+            textAction.setText(R.string.lemon_drink)
+        }
+
+        if (lemonadeState == RESTART) {
+            lemonImage!!.setImageResource(R.drawable.lemon_restart)
+            textAction.setText(R.string.lemon_empty_glass)
+        }
+
+    }
+
+    /**
+     * === NE PAS MODIFIER CETTE MÉTHODE ===
+     *
+     * Un clic long sur l'image du citron montrera combien de fois le citron a été pressé.
+     */
+    private fun showSnackbar(): Boolean {
+        if (lemonadeState != SQUEEZE) {
+            return false
+        }
+        val squeezeText = getString(R.string.squeeze_count, squeezeCount)
+        Snackbar.make(
+            findViewById(R.id.constraint_Layout),
+            squeezeText,
+            Snackbar.LENGTH_SHORT
+        ).show()
+        return true
+    }
+}
+
+/**
+ * Une classe Lemon Tree avec une méthode "pick" pour cueillir un citron. La "taille" du citron est aléatoire
+ * et détermine combien de fois un citron doit être pressé avant d'obtenir de la limonade.
+ */
+class LemonTree {
+    fun pick(): Int {
+        return (2..4).random()
+    }
+}
